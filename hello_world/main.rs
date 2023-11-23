b4a8b26ef7ea750e3a0dd2de46f391056d78c3c3
krobbi <70552146+krobbi@users.noreply.github.com>
2023-11-23 17:12:20 +0000

    Rust book chapter 1.2

---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+// The 'main' function is always run and has no parameters or return type.
+fn main() {
+    // Macros end with an exclamation mark.
+    // Semicolons are used at the end of most statements.
+    println!("Hello, world!");
+}
