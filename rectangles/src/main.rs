62c04e926840976ba50c46b217c730481532e757
krobbi <70552146+krobbi@users.noreply.github.com>
2023-11-26 14:09:53 +0000

    Rust book chapter 5.3

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,24 +1,56 @@
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
+impl Rectangle {
+    // Methods without self references can be used as constructors/statics.
+    fn new(width: u32, height: u32) -> Self {
+        Self {
+            width,
+            height,
+        }
+    }
     
-    println!("rect1 is {:?}", rect1);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+    
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+    // Fields can share names with methods.
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
-// Get the area of a rectangle.
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle::new(30, 50);
+    
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+    
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+    
+    let rect2 = Rectangle::new(10, 40);
+    let rect3 = Rectangle::new(60, 45);
+    
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect2.can_hold(&rect3));
+    
+    let sq = Rectangle::square(3);
+    println!("The area of the square is {} square pixels.", sq.area());
 }
